2185d901dd28960d7c722b60dced794f95cb235a
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-03-13 10:13:20 +0800

    Updated url

--- SecondProject/SecondPRoject/urls.py
+++ SecondProject/SecondPRoject/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from BasicApp import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('basicapp/hello/',views.sayHello)
+    path('basicapp/', include("BasicApp.urls"))
 ]
